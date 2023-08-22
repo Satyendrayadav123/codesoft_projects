0062b8527cd058c8da53138700b80e350b7aa82d
Satyendrayadav123 <118504416+Satyendrayadav123@users.noreply.github.com>
2023-08-22 22:29:38 +0530

    Add files via upload

---
+++ Projects/RandomNumberGame.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+public class RandomNumberGame {
+    public static void main(String[] args) {
+        int answer , guess ;
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        System.out.println("Enter a number between 0 and 100 :");
+        guess = sc.nextInt();
+        answer = rand.nextInt(100);
+
+        if(guess>answer){
+            System.out.println("Your guess is larger than answer, please make a guess little smaller");
+        } else if (guess<answer) {
+            System.out.println("Your Guess is smaller than answer, please make guess larger");
+        } else {
+            System.out.println("Congratulations you made a correct guess.");
+        }
+    }
+}
